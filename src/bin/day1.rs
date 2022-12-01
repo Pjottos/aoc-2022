0988d4516ef9dad9f66f98bd3a81784387cd59d9
Pjottos <pjottos@proton.me>
2022-12-01 14:25:00 +0100

    Day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -3,7 +3,18 @@ use aoc_2022::Harness;
 fn main() {
     Harness::begin()
         .day(1)
-        .extract(|text| {})
-        .run_part(1, |_| {})
-        .run_part(2, |_| {});
+        .extract(|text| {
+            text.split("\n\n").map(|block| {
+                block
+                    .lines()
+                    .map(|line| line.parse::<u32>().unwrap())
+                    .sum::<u32>()
+            })
+        })
+        .run_part(1, |sums| sums.clone().max().unwrap())
+        .run_part(2, |sums| {
+            let mut sums = sums.clone().collect::<Vec<_>>();
+            sums.sort_unstable();
+            sums.iter().rev().take(3).sum::<u32>()
+        });
 }
