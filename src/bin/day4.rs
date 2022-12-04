c38db5154d7f650f9b94e503fb2f0622cecca2fe
Pjottos <pjottos@proton.me>
2022-12-04 17:18:01 +0100

    Optimize day 4

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -3,30 +3,73 @@ use aoc_2022::*;
 fn main() {
     Harness::begin()
         .day(4)
-        .extract(|text| {
-            text.lines().map(|line| {
-                let mut ranges = line.split(',').map(|range| {
-                    let (first, last) = range.split_once('-').unwrap();
-                    fast_u32_parse(first)..fast_u32_parse(last) + 1
-                });
-                (ranges.next().unwrap(), ranges.next().unwrap())
-            })
+        .extract(|text| text.lines().map(parse_ids))
+        .run_part(1, |lines| {
+            //let mut chunks = text.lines().array_chunks::<32>();
+
+            //let counts = chunks
+            //    .by_ref()
+            //    .map(|chunk| {
+            //        let mut vectors = [u8x32::default(); 4];
+            //        for (j, line) in chunk.into_iter().enumerate() {
+            //            let ids = parse_ids(line);
+            //            for i in 0..4 {
+            //                vectors[i].as_mut_array()[j] = ids[i];
+            //            }
+            //        }
+            //        vectors
+            //    })
+            //    .fold(u8x32::default(), |counts, v| {
+            //        let mask = (v[0].simd_ge(v[2]) & v[1].simd_le(v[3]))
+            //            | (v[2].simd_ge(v[0]) & v[3].simd_le(v[1]));
+            //        counts + (mask.to_int().cast() & u8x32::splat(1))
+            //    });
+
+            //let mut count = counts
+            //    .as_array()
+            //    .iter()
+            //    .fold(0, |count, &c| count + c as u32);
+
+            //count += chunks
+            //    .into_remainder()
+            //    .into_iter()
+            //    .flat_map(|lines| lines.map(parse_ids))
+
+            lines
+                .map(|v| ((v[0] >= v[2] && v[1] <= v[3]) || (v[2] >= v[0] && v[3] <= v[1])) as u32)
+                .sum::<u32>()
         })
-        .run_part(1, |pairs| {
-            pairs
-                .filter(|(a, b)| {
-                    (a.start >= b.start && a.end <= b.end) || (b.start >= a.start && b.end <= a.end)
-                })
-                .count()
-        })
-        .run_part(2, |pairs| {
-            pairs
-                .filter(|(a, b)| {
-                    a.contains(&b.start)
-                        || a.contains(&(b.end - 1))
-                        || b.contains(&a.start)
-                        || b.contains(&(a.end - 1))
-                })
-                .count()
+        .run_part(2, |lines| {
+            lines
+                .map(|v| (v[0] <= v[3] && v[1] >= v[2]) as u32)
+                .sum::<u32>()
         });
+}
+
+#[inline]
+fn parse_ids(line: &str) -> [u8; 4] {
+    let mut bytes = line.as_bytes().iter().copied().rev();
+    let mut res = [0; 4];
+
+    for i in (0..4).rev() {
+        // Parse at most 2 digits correctly
+        let mut multiplier = 1;
+        let mut num = 0;
+        loop {
+            match bytes.next() {
+                Some(byte) => {
+                    if byte < b'0' {
+                        break;
+                    } else {
+                        num += (byte - b'0') * multiplier;
+                        multiplier = 10;
+                    }
+                }
+                None => break,
+            }
+        }
+        res[i] = num;
+    }
+
+    res
 }
