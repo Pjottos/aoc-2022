583b4348c8004e610dba0bc59ea713baebf32121
Pjottos <pjottos@proton.me>
2022-12-06 03:02:40 +0100

    Optimize day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,11 +1,39 @@
 use aoc_2022::*;
 
+use std::iter;
+
 fn main() {
     Harness::begin()
         .day(1)
         .extract(|text| {
-            text.split("\n\n")
-                .map(|block| block.lines().map(fast_u32_parse).sum::<u32>())
+            let mut bytes = text.as_bytes().iter().copied().rev().skip(1);
+            iter::from_fn(move || {
+                if bytes.len() == 0 {
+                    return None;
+                }
+
+                let mut sum = 0;
+                let mut next = bytes.next().unwrap();
+                loop {
+                    if next == b'\n' {
+                        next = bytes.next().unwrap();
+                        if next == b'\n' {
+                            break Some(sum);
+                        }
+                    }
+                    for multiplier in [1, 10, 100, 1000] {
+                        sum += (next - b'0') as u32 * multiplier;
+                        next = bytes.next().unwrap_or(0);
+                    }
+                    if next & 0x10 != 0 {
+                        sum += (next - b'0') as u32 * 10000;
+                        next = bytes.next().unwrap_or(0);
+                    }
+                    if next == 0 {
+                        break Some(sum);
+                    }
+                }
+            })
         })
         .run_part(1, |sums| sums.max().unwrap())
         .run_part(2, |sums| {
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
 mod harness;
 
 pub use harness::Harness;
-
-#[inline]
-pub fn fast_u32_parse(text: &str) -> u32 {
-    // Numbers with at most 8 decimal digits are guaranteed to fit in a u32
-    const POWERS_OF_10: [u32; 9] = [
-        1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000,
-    ];
-    text.as_bytes()
-        .iter()
-        .rev()
-        .take(8)
-        .map(|b| b - b'0')
-        .enumerate()
-        .fold(0, |sum, (i, digit)| sum + digit as u32 * POWERS_OF_10[i])
-}
